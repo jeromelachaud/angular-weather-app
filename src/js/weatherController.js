7e9625e00a64817d0c5eed8caa6073fb25ada864
Jerome Lachaud <jerome.lachaud@gmail.com>
2016-08-31 18:55:18 +0200

    Update wether controller

--- src/js/weatherController.js
+++ src/js/weatherController.js
@@ -1,4 +1,6 @@
-function weatherController($scope) {
+import moment from 'moment';
+
+function weatherController($scope, $http) {
   const weatherApi = {
     apiKey: '0574fdb7238ef696712ccd89746bb0e0',
     baseUrl: 'http://api.openweathermap.org/data/2.5/weather?q=',
@@ -15,9 +17,18 @@ function weatherController($scope) {
 
   const weatherUrl = `${baseUrl}${city}&appid=${apiKey}&units=${unit}`;
 
-  console.log(weatherUrl);
-  console.log($scope);
-  $scope.message = 'Two birds killed with one stone!'
+  $http.get(weatherUrl)
+  .success((response) => {
+    $scope.city = response.name;
+    $scope.humidity = response.main.humidity;
+    $scope.temperature = Number((response.main.temp).toFixed(1));
+    $scope.wind = response.wind.speed;
+    $scope.description = response.weather[0].description;
+    $scope.main = response.weather[0].main;
+    $scope.icon = response.weather[0].icon;
+    $scope.sunrise = moment.parseZone(response.sys.sunrise * 1000).local().format('HH:mm');
+    $scope.sunset = moment.parseZone(response.sys.sunset * 1000).local().format('HH:mm');
+  });
 }
 
-export { weatherController }
+export { weatherController };
