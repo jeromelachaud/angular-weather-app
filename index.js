5c7d3450983f51cac3e5c5bd3ae19df6a55db395
Jerome Lachaud <jerome.lachaud@gmail.com>
2016-08-31 18:53:38 +0200

    Remove flickr stuff

--- index.js
+++ index.js
@@ -3,17 +3,7 @@ import { weatherController } from './src/js/weatherController';
 const app = angular.module('app', [])
 app.controller('weatherController', ['$scope', weatherController])
 
-const {
-  flickrApiKey,
-  flickrApiBaseUrl,
-  method,
-  text,
-  group_id,
-  photos_per_page,
-  number_of_page,
-} = flickrApi;
 
-const flickrUrl = `${flickrApiBaseUrl}${method}&api_key=${flickrApiKey}&text=${text}&group_id=${group_id}&per_page=${photos_per_page}&${number_of_page}=1&format=json&nojsoncallback=1`;
 
 fetchUrl(flickrUrl)
 .then((response) => {
