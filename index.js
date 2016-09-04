7680ca2c109747c2876ba3258fedcada87d09041
Jerome Lachaud <jerome.lachaud@gmail.com>
2016-09-04 18:45:07 +0100

    Delete flickr API data

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 import angular from 'angular';
 import { weatherController } from './src/js/weatherController';
-import { flickrController } from './src/js/flickrController';
 import { formDataFactory } from './src/js/formDataFactory';
 import { weatherApiFactory } from './src/js/weatherApiFactory';
-import { flickrApiFactory } from './src/js/flickrApiFactory';
 
 const svgSpriteInjector = require('./src/js/lib/svg-sprite-injector.js');
 const svgSpriteContainer = document.getElementById('svgSpriteContainer');
@@ -17,6 +15,4 @@ const app = angular.module('app', [])
   });
 app.factory('formDataFactory', [formDataFactory]);
 app.factory('weatherApiFactory', [weatherApiFactory]);
-app.factory('flickrApiFactory', [flickrApiFactory]);
 app.controller('weatherController', ['$scope', '$http', 'weatherApiFactory', 'formDataFactory', weatherController]);
-app.controller('flickrController', ['$scope', '$http', 'flickrApiFactory', flickrController]);
